d222e6e5b9ec3c696850a6df84416f8b0720c00f
Muraleekrishna Gopinathan <muraleekrishna.g@multunus.com>
2015-04-21 11:02:21 +0530

    Add entry button on monthly summary and leaderboard

--- js/moveit.js
+++ js/moveit.js
@@ -1,3 +1,9 @@
+$(function(){
+  $('.add-entry-btn').on('click', 'i' , function(){
+    window.location = "index.html";
+  });
+});
+
 var disableSaveButtonWith = function(form, text){
   var saveBtn = $(form).find('button[type=submit]')[0];
   saveBtn.disabled = true;
