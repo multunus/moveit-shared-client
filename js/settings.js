f16ee03bfc94f0e4a5d8987b2fbd0ee0586056d7
Midhun Krishna <midhun.k@multunus.com>
2015-04-22 15:44:07 +0530

    add settings page and test mode

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,16 @@
 var settings = {
-  server:{
+  test:{
+    baseUrl: "https://pink-server.herokuapp.com/",
+    apiUrl: "https://pink-server.herokuapp.com/api/"
+  },
+  production:{
     baseUrl: "https://mov3it.herokuapp.com/",
     apiUrl: "https://mov3it.herokuapp.com/api/"
+  },
+
+  getSetting: function(setting){
+    var settings = JSON.parse(localStorage.getItem("settings")) || {};
+    var mode = settings.mode || "production";
+    return this[mode][setting];
   }
 };
